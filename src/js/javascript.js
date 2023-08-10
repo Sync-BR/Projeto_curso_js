33b4056a152c3e19c7e28f2ce858e663b37b0eeb
sync <eduardofreitasfr@gmail.com>
2023-08-10 19:56:45 -0300

    Subindo projeto calculadora

---
+++ src/js/javascript.js
@@ -0,0 +1,10 @@
+// Fazer Calculo
+function somar(num1,num2){
+    num1 = 5
+    num2 = 5
+    total = num1 + num2
+    var textoresult = document.getElementById("resultado");
+    resultado.textContent = total;
+    
+}
+
